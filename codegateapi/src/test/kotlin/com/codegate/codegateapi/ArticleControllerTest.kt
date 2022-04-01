27be2f913580987249e31515c0a70ae2f4751ddc
ShionKoga <shion_koga@mail.toyota.co.jp>
2022-04-01 23:24:22 +0900

    ArticleControllerTestのAssertionを追加

--- codegateapi/src/test/kotlin/com/codegate/codegateapi/ArticleControllerTest.kt
+++ codegateapi/src/test/kotlin/com/codegate/codegateapi/ArticleControllerTest.kt
@@ -3,12 +3,14 @@ package com.codegate.codegateapi
 import org.junit.jupiter.api.Test
 import org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 class ArticleControllerTest {
     @Test
-    fun `api articles end point returns hello spring boot`() {
+    fun `api articles end point returns something`() {
         standaloneSetup(ArticleController())
             .build()
             .perform(get("/api/articles"))
+            .andExpect(status().isOk)
     }
 }
